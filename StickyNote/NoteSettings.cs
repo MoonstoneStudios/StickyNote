8ccf1a63aad214eb77270f8137d6b9b6b3cf9b78
Moonstone Studios <owenjh12345@gmail.com>
2022-06-20 16:49:38 -0700

    Renamed MainWindow to StickyNote
    Also added colored Emoji support using Emoji.WPF!

---
+++ StickyNote/NoteSettings.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Documents;
+using System.Windows.Media;
+
+namespace StickyNote
+{
+    /// <summary>
+    /// The settings for the note.
+    /// </summary>
+    public class NoteData
+    {
+        /// <summary>
+        /// The background color of the note.
+        /// </summary>
+        public SolidColorBrush BackgroudColor { get; set; } = new SolidColorBrush()
+        {
+            Color = (Color)ColorConverter.ConvertFromString("#FFFFE66E")
+        };
+
+        /// <summary>
+        /// The font to use.
+        /// </summary>
+        public FontFamily FontFamily { get; set; } = new FontFamily("Segoe UI");
+
+        /// <summary>
+        /// The size of the font in the editor.
+        /// </summary>
+        public float FontSize { get; set; } = 14;
+
+        /// <summary>
+        /// The position of the note.
+        /// <para>
+        /// If the value is <c>null</c> then set the position to <see cref="WindowStartupLocation.CenterScreen"/>
+        /// </para>
+        /// </summary>
+        public Point? Position { get; set; } = null;
+        
+        /// <summary>
+        /// The title of the note.
+        /// </summary>
+        public string Title { get; set; }
+
+        /// <summary>
+        /// The content of the editor.
+        /// </summary>
+        public FlowDocument Content { get; set; }
+
+        /// <summary>
+        /// If the note is pinned.
+        /// </summary>
+        public bool Pinned { get; set; } = true;
+
+        /// <summary>
+        /// The size of the note.
+        /// </summary>
+        public Point Size { get; set; } = new Point(450, 450);
+
+    }
+}
---
+++ StickyNote/StickyNote.xaml.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Effects;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using EImage = Emoji.Wpf.Image;
+
+namespace StickyNote
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class StickyNoteWindow : Window
+    {
+        /// <summary>
+        /// The settings for the note.
+        /// </summary>
+        public NoteData Data { get; set; } = new NoteData();
+
+        public StickyNoteWindow()
+        {
+            InitializeComponent();
+        }
+
+        public StickyNoteWindow(bool deserialized) : this()
+        {
+            // TODO
+        }
+
+        /// <summary>
+        /// Pin the window.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void PinButtonClick(object sender, RoutedEventArgs e)
+        {
+            Topmost = !Topmost;
+
+            Button b = (Button)sender;
+
+            if (Topmost)
+            {
+                // change the image inside the pin.
+                EImage.SetSource((DependencyObject)b.Content, "📍");
+                b.Effect = null;
+            }
+            else
+            {
+                // change the image inside the pin.
+                EImage.SetSource((DependencyObject)b.Content, "📌");
+                // add effect
+                b.Effect = new DropShadowEffect()
+                {
+                    Opacity = 0.6f,
+                    BlurRadius = 3
+                };
+            }
+
+            Data.Pinned = Topmost;
+
+        }
+
+        /// <summary>
+        /// Close the note.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ExitButtonClick(object sender, RoutedEventArgs e)
+        {
+            // Save(); TODO
+            Close();
+            // shutdown if shift held.
+            if (Keyboard.IsKeyDown(Key.LeftShift))
+                Application.Current.Shutdown();
+        }
+
+        /// <summary>
+        /// Make a new window close to the current.
+        /// </summary>
+        private void NewNoteButtonClick(object sender, RoutedEventArgs e)
+        {
+            // https://stackoverflow.com/a/2734831
+            // https://stackoverflow.com/a/3244693
+            StickyNoteWindow note = new StickyNoteWindow();
+            note.Show();
+
+            Point thisNote = PointToScreen(new Point(Left, Top));
+
+            Point newNote = new Point(thisNote.X + 50, thisNote.Y + 50);
+
+            newNote = PointFromScreen(newNote);
+
+            note.Left = newNote.X;
+            note.Top = newNote.Y;
+
+            note.Data.Position = newNote;
+
+        }
+
+        /// <summary>
+        /// Move the note
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void MoveWhenDragged(object sender, MouseButtonEventArgs e)
+        {
+            // only move with left mouse
+            if (e.LeftButton == MouseButtonState.Pressed && e.RightButton == MouseButtonState.Released)
+                DragMove();
+        }
+
+    }
+}
